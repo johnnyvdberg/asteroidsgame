d400be43ac83239b4912a8e30c14d9bdc5d84105
Davey <Dave.pirate@gmail.com>
2013-05-27 13:20:13 +0200

    WIN
    Vice city

--- html5/menu.js
+++ html5/menu.js
@@ -1,26 +1,78 @@
 var menuimg;
 var timer = null; 
 var mousePos = null;
+var soundManager;
+var mouseDown;
+var soundNames = [
+	  { id: 'menumusic', url:'music/01-Vangelis-Heaven-and-Hell.mp3'},
+	  { id: 'click', url:'sounds/menu/clicksound.mp3'}, 
+	  { id: 'hover1', url:'sounds/menu/hoversound.mp3'},
+	  { id: 'hover2', url:'sounds/menu/hoversound.mp3'},
+	  { id: 'hover3', url:'sounds/menu/hoversound.mp3'},
+	  { id: 'hover4', url:'sounds/menu/hoversound.mp3'}
+	]
+	
+var hoverIndicator = {
+  showing: 0, // % 0-100
+  cy : 0,
+  ty : 0	
+}
 
-function menuLoadImages(){
+function menuLoad(){
 	var loader = new PxLoader();
 	menuimg = loader.addImage('images/menu/mainmenu.png');
-	loader.addCompletionListener(function(){ menuLoaded(); });
+	loader.addCompletionListener(function(){ sharedLoad(); });
 	loader.start();
+	// initialize the sound manager 
+	soundManager.setup({
+		url: 'soundmanager2/swf/',
+		// preferFlash: false,
+		onready: function() { loadSounds(); },
+		ontimeout: function() { l('error loading soundmanager.'); }
+	});
 }
 
 window.onload = function(e){
-	setTimeout("menuLoadImages()",1000); // show off loader 1 second longer, remove later
+    setTimeout("menuLoad()",1000); // show off loader 1 second longer, remove later
+};
+
+var tmpsl = 0;
+function sharedLoad(){ // laad de 2 dingen teglijk, wacht tot ze bijden klaar zijn
+    tmpsl++;
+    if(tmpsl>1){ menuLoaded(); }	
 };
 
 function menuLoaded(){
 	canvas = document.createElement("canvas");
-	canvas.width=1280; canvas.height=720; // we should maybe build this to suit resizing
+	canvas.width=1280; canvas.height=800; // we should maybe build this to suit resizing
 	ctx = canvas.getContext("2d");
 	timer = setInterval("menuUpdate();",5);	
+	menuUpdate();
+	then = Date.now();
 	document.body.appendChild(canvas); 
-	
-	canvas.addEventListener('mousemove', function(evt) { mousePos = getMousePos(canvas, evt); }, false); 
+	menuPlayMusic();
+	canvas.addEventListener('mousemove', function(evt) { mousePos = getMousePos(canvas, evt); }, false);
+	canvas.addEventListener('mousedown', function(e) { mouseDown = true; },false); 
+	canvas.addEventListener('mouseup', function(e) { mouseDown = false; },false); 
+}
+
+function menuPlayMusic(){
+    soundManager.setVolume('menumusic',10);
+    soundManager.play('menumusic',{ onfinish: function() { menuPlayMusic(); } });	
+}
+
+function menuPlayClick(){
+    soundManager.setVolume('click',100);
+    soundManager.play('click',{ onfinish: function() { menuPlayMusic(); } });	
+}
+
+function menuPlayHoverSound(i){
+	if(soundManager.getSoundById('hover'+i).playState==1){
+	  if(i<4){ menuPlayHoverSound(i+1); }  
+	}else{
+      soundManager.setVolume('hover'+i,90);
+      soundManager.play('hover'+i,{ onfinish: function() { } });
+	}
 }
 
 
@@ -32,19 +84,94 @@ function getMousePos(canvas, evt) { var rect = canvas.getBoundingClientRect(); r
 function l(e){ console.log(e); }
 
 function menuUpdate(){
+    var now = Date.now();
+	var delta = (now - then);
+	then = now;
 	if(mousePos!=undefined){
-	  l(mousePos);
-	  if((mousePos.y>468) && (mousePos.x>789) && (mousePos.x<1024) && (mousePos.y<515)){ 
-		  menuArcadeMode();	
-	  }
+	  	if((mousePos.y>475) && (mousePos.x>789) && (mousePos.x<1024) && (mousePos.y<511)){ // arcade mode
+			menuHover(475, delta); if(mouseDown){ menuArcadeMode(); }
+	  	}else if((mousePos.y>512) && (mousePos.x>789) && (mousePos.x<1024) && (mousePos.y<548)){  // time attack
+			menuHover(512, delta);	
+	  	}else if((mousePos.y>555) && (mousePos.x>789) && (mousePos.x<1024) && (mousePos.y<590)){ // slingshot
+			menuHover(555, delta);	
+	  	}else if((mousePos.y>659) && (mousePos.x>789) && (mousePos.x<1024) && (mousePos.y<695)){ // option
+			menuHover(659, delta);	
+	  	}else if((mousePos.y>699) && (mousePos.x>789) && (mousePos.x<1024) && (mousePos.y<735)){ // quit
+			menuHover(699, delta);	
+	  	}else{
+		    menuHoverOut(delta);	
+		}
+		menuMove(delta);
 	}
-    ctx.drawImage(menuimg, 0, 0);	
+    ctx.drawImage(menuimg, 0, 0);
+	// draw menu item	
+	ctx.beginPath();
+	ctx.rect(790, hoverIndicator.cy, 234, 35);
+	ctx.fillStyle = "rgba(255,255,255,"+(hoverIndicator.showing/1000)+")";
+	ctx.fill();
+}
+
+function menuMove(dt){
+  if((hoverIndicator.cy+dt)<hoverIndicator.ty){
+	hoverIndicator.cy += dt;  
+  }else if ((hoverIndicator.cy-dt)>hoverIndicator.ty){
+	hoverIndicator.cy -= dt;  
+  }else{
+	hoverIndicator.cy = hoverIndicator.ty;  
+  }
+}
+
+function menuHover(y,dt){
+  var oldy = hoverIndicator.ty;	
+  if(hoverIndicator.showing>0){
+	hoverIndicator.ty = y;  
+  }else{
+	hoverIndicator.cy = y;
+	hoverIndicator.ty = y; 
+	oldy = -1; //reset so sound always play on rehover  
+  }
+  hoverIndicator.showing += (dt*3);
+  if(hoverIndicator.showing>500){ hoverIndicator.showing = 500; }
+  if(y!=oldy){ menuPlayHoverSound(1); }
+}
+
+function menuHoverOut(dt){
+  hoverIndicator.showing -= (dt*3);	
+  if(hoverIndicator.showing<0){ hoverIndicator.showing = 0; }
 }
 
 function menuArcadeMode(){
+	menuPlayClick();
     canvasHide();	
     stopTimer();
     setTimeout("gameLoadImages();",1000); // show off loader 1 second longer, remove later
+}
+
+function loadSounds(){
+	var loader = new PxLoader(), 
+	i, len, url, n; 
+	n = 0;
+	 
+	// queue each sound for loading 
+	for(i=0, len = soundNames.length; i < len; i++) { 
+	 
+		// see if the browser can play m4a 
+		url = soundNames[i].url;
+		if (!soundManager.canPlayURL(url)) { 
+			continue; // can't be played 
+		} 
+		// queue the sound using the name as the SM2 id 
+		loader.addSound(soundNames[i].id, url); 
+	} 
+	len = soundNames.length;
+	 
+	// listen to load events 
+	loader.addProgressListener(function(e) { 
+	  n++;
+	  if(n>=len){ sharedLoad(); }  
+	}); 
+	 
+	loader.start(); 	
 }
 
 function stopTimer(){ if(timer!=null){ window.clearInterval(timer); timer = null; } } // stop updaten 
